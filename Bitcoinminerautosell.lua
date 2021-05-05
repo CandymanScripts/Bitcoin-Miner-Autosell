aefd89d92304b85e3f43e7878a70ef1f6f2dc7b5
CandymanScripts <83679084+CandymanScripts@users.noreply.github.com>
2021-05-05 01:33:39 -0400

    Update Bitcoinminerautosell.lua

--- Bitcoinminerautosell.lua
+++ Bitcoinminerautosell.lua
@@ -1,19 +1,24 @@
 --hi this is my first script other then just jumphight and walkspeed
 
+
 AutoOverclock = true
 Overclock = 100 --change from 5 to 150 it is how much efficincy
 while (true)  do
     wait(1)
     game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(true)
     game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(false)
-    
+    if(AutoOverclock) then
+        
+        game:GetService("ReplicatedStorage").Events.Overclk:InvokeServer(Overclock)
+
+        end
 end
-while (AutoOverclock) do
-wait(10)
+
 game:GetService("ReplicatedStorage").Events.Overclk:InvokeServer(Overclock)
+wait(10)
+
 
     
-    end
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
 vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
