c9747f86db48d435360f5523c31606abb9a0c8c5
CandymanScripts <83679084+CandymanScripts@users.noreply.github.com>
2021-05-06 22:42:34 -0400

    Update Bitcoinminerautosell.lua

--- Bitcoinminerautosell.lua
+++ Bitcoinminerautosell.lua
@@ -3,22 +3,23 @@
 
 AutoOverclock = true
 Overclock = 100 --change from 5 to 150 it is how much efficincy
-
-while (true)  do
-    wait(0.5)
-    game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(true)
-    wait(0.5)
-    game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(false)
-    
-    if(AutoOverclock)then
-        game:GetService("ReplicatedStorage").Events.Overclk:InvokeServer(Overclock)
-
-        end
-end
-
 local vu = game:GetService("VirtualUser")
 game:GetService("Players").LocalPlayer.Idled:connect(function()
 vu:Button2Down(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 wait(1)
 vu:Button2Up(Vector2.new(0,0),workspace.CurrentCamera.CFrame)
 end)
+
+while (true)  do
+    wait(0.5)
+    game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(true)
+    wait(0.5)
+    game:GetService("ReplicatedStorage").Events.ExchangeMoney:FireServer(false)
+    --game.Workspace.PLACEHOLDER.PLACEHOLDER.CFrame = CFrame.new(2048, 2048, 2048)
+    --game.Workspace.PLACEHOLDER.PLACEHOLDER.Size = Vector3.new(2048, 2048, 2048)
+    if(AutoOverclock)then
+        game:GetService("ReplicatedStorage").Events.Overclk:InvokeServer(Overclock)
+
+        end
+end
+
